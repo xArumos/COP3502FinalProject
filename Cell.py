ba063670b82f491e2ddb07aec3770b2fc7c28545
Justin <jqperkins05@gmail.com>
2025-04-13 02:42:16 -0400

    Added Cell class and put some placeholder functions.

---
+++ Cell.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from constants import *
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        pass
+
+    def draw(self):
+        pass
